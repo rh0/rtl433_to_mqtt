e36cacc5a5f6f6af8ad416849914e760ea23acaf
rho <ryan@theoleschool.com>
2020-08-18 10:12:24 -0500

    adding auth options to shell script

--- rtl433_to_mqtt.sh
+++ rtl433_to_mqtt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Starting rtl_433 to MQTT!"
-echo "Running command: rtl_433 -v -M time:unix:usec:utc -C customary -F \"mqtt://${MQTT_HOST}:${MQTT_PORT},events=rtl_433/sensor[/model]\""
+echo "Running command: rtl_433 -v -M time:unix:usec:utc -C customary -F \"mqtt://${MQTT_HOST}:${MQTT_PORT}user=${MQTT_USER},pass=${MQTT_PASS},events=rtl_433/events[/model],devices=rtl_433/devices\""
 
-rtl_433 -v -M time:unix:usec:utc -C customary -F "mqtt://${MQTT_HOST}:${MQTT_PORT},events=rtl_433/sensor[/model]"
+rtl_433 -v -M time:unix:usec:utc -C customary -F "mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},events=rtl_433/events[/model],devices=rtl_433/devices"
