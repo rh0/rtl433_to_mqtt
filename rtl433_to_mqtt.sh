519121b8898c74f66ab5dd415c7aac79be3b3c50
rho <ryan@theoleschool.com>
2022-04-25 11:08:11 -0500

    removing debug

--- rtl433_to_mqtt.sh
+++ rtl433_to_mqtt.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Starting rtl_433 to MQTT!"
 echo "Data will be published to rtl_433 topic."
-echo "mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},events=rtl_433/sensors[/model]"
 
 rtl_433 -v -M time:unix:usec:utc -C customary -F "mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},events=rtl_433/sensors[/model]"
